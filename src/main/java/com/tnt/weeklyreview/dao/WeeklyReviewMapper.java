c3a031e9bd85484a9f10b3c60dd1b2d1540aea50
乘义 <zhaojun.wzj@alibaba-inc.com>
2015-12-25 23:19:31 +0800

    1111

---
+++ src/main/java/com/tnt/weeklyreview/dao/WeeklyReviewMapper.java
@@ -0,0 +1,20 @@
+package com.tnt.weeklyreview.dao;
+
+import com.tnt.weeklyreview.model.Task;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by zhaojunwzj on 12/24/15.
+ */
+public interface WeeklyReviewMapper {
+    int save(Task task);
+
+    int update(Task task);
+
+    List<Task> getTasks(Long userId, int beginDate, int endDate);
+
+    List<Task> getTasks4Day(Map map);
+    
+}
---
+++ src/main/java/com/tnt/weeklyreview/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.tnt.weeklyreview.service.impl;
+
+import com.tnt.weeklyreview.dao.UserInfoMapper;
+import com.tnt.weeklyreview.model.UserInfo;
+import com.tnt.weeklyreview.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserInfoMapper userInfoMapper;
+
+	
+	public UserInfo getUserById(int id) {
+		return userInfoMapper.selectByPrimaryKey(id);
+	}
+
+	public List<UserInfo> getUsers() {
+		return userInfoMapper.selectAll();
+	}
+
+	public int insert(UserInfo userInfo) {
+		
+		int result = userInfoMapper.insert(userInfo);
+		
+		System.out.println(result);
+		return result;
+	}
+
+}
